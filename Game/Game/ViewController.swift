f0dfd20840c9049739649c4ec9a7ea90c1c83b70
Moskalev Nikita <nikmosyl@gmail.com>
2017-03-07 16:00:48 +0300

    Пройден первый и второй уроки

---
+++ Game/Game/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Game
+//
+//  Created by Никита Москалёв on 07.03.17.
+//  Copyright © 2017 Moscorp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        NSLog("ok1")                            //Вывод сообщения в консоль при загрузке первого окна
+                                                // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Game/ViewController2.swift
@@ -0,0 +1,25 @@
+//
+//  UIView2.swift
+//  Game
+//
+//  Created by Никита Москалёв on 07.03.17.
+//  Copyright © 2017 Moscorp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        NSLog("ok2")                                //Вывод сообщения в консоль при загрузке второго окна
+                                                    // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+                                                    // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
